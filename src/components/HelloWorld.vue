99ed48eaec901a9a85825058686645b4b774487f
kotobukid <kotobuki@daqua.jp>
2022-03-15 17:36:26 +0900

    コンポーネントを試作

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,11 +1,17 @@
 <template lang="pug">
     svg(width="1000" height="600" viewBox="0 0 1000 600")
+        node-box(text="ほげ")
 </template>
 
 <script lang="ts">
 import {Component, Prop, Vue} from 'vue-property-decorator';
+import NodeBox from "@/components/node_box.vue";
 
-@Component
+@Component({
+    components: {
+        'node-box': NodeBox
+    }
+})
 export default class HelloWorld extends Vue {
     @Prop() private msg!: string;
 }
@@ -14,5 +20,6 @@ export default class HelloWorld extends Vue {
 <style scoped lang="less">
 svg {
     border: 1px solid grey;
+    background-color: #efefef;
 }
 </style>
---
+++ src/components/node_box.vue
@@ -0,0 +1,31 @@
+<template lang="pug">
+    g.node
+        rect.frame(x="0" y="0" width="200" height="40")
+        text.text(v-text="text" x="100" y="28")
+</template>
+
+<script lang="ts">
+
+import {Component, Prop, Vue} from 'vue-property-decorator';
+
+@Component
+export default class NodeBox extends Vue {
+    @Prop() private text!: string;
+}
+
+</script>
+
+<style lang="less" scoped>
+rect.frame {
+    fill: #ccfdf7;
+    stroke: black;
+    stroke-width: 1px;
+}
+
+.text {
+    text-anchor: middle;
+    font-size: 1rem;
+    line-height: 1rem;
+}
+
+</style>
